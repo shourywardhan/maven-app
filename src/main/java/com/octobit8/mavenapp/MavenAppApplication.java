7b6528c1d973adac5da46bd85cdcefc2ebf9e9af
shourywardhan <shourywardhan24@gmail.com>
2024-02-23 10:26:56 +0530

    Initial commit

---
+++ src/main/java/com/octobit8/mavenapp/MavenAppApplication.java
@@ -0,0 +1,13 @@
+package com.octobit8.mavenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/octobit8/mavenapp/MavenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octobit8.mavenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
